0c21b295dc01ce2ddf0b07856914a897ca97cca0
rozay10 <raadeniyi3@gmail.com>
2021-04-03 21:55:25 +0100

    react-hook-form error

--- pages/app/create-page.tsx
+++ pages/app/create-page.tsx
@@ -11,30 +11,9 @@ const CreatePage = (props) => {
     },
   } = props;
 
-<<<<<<< HEAD
   return (
     <Dashboard>
       <AddNewPage siteId={sites[0].id} />
-=======
-const CreatePage = (props) => {
-  const site: any = useAppContext();
-  const { data, error, loading } = useQuery(PAGES_QUERY, {
-    variables: { siteId: site.id },
-  });
-  // console.log(site);
-  // console.log('DATA', data);
-  if (error) {
-    console.error(error.message);
-    console.error(error.graphQLErrors);
-  }
-
-  if (loading) {
-    return <div>Loading</div>;
-  }
-  return (
-    <Dashboard>
-      <AddNewPage site={props.sites.data.sites[0].id} />
->>>>>>> 07bfe90a812512a6b5e40e9a391eddb741d481f2
     </Dashboard>
   );
 };
